baaf98a87dd37042b882abfb5a54122667218388
zhangxiaosong <848967151@qq.com>
2023-08-21 10:30:20 +0800

    .

---
+++ lib/src/AudioInfo.dart
@@ -0,0 +1,27 @@
+class AudioInfo {
+  String url;
+  String title;
+  String desc;
+  String coverUrl;
+
+  AudioInfo(this.url,
+      {required this.title, required this.desc, required this.coverUrl});
+
+  AudioInfo.fromJson(Map<String, dynamic> json)
+      : url = json['url'],
+        title = json['title'],
+        desc = json['desc'],
+        coverUrl = json['coverUrl'];
+
+  Map<String, String> toJson() => {
+        'url': url,
+        'title': title,
+        'desc': desc,
+        'coverUrl': coverUrl,
+      };
+
+  @override
+  String toString() {
+    return 'AudioInfo{url: $url, title: $title, desc: $desc, coverUrl: $coverUrl}';
+  }
+}
